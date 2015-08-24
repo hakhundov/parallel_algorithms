71d873cbbc73ceff726295ec294144292c566673
Haji <hajiakhundov@gmail.com>
2015-08-24 17:25:42 +0200

    ex C, pthreads DONE

--- C/p_main.c
+++ C/p_main.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <pthread.h>
-#include <unistd.h>
-
 
 #define SIZE 			16
 #define NUM_THREADS     16
@@ -15,62 +12,62 @@ int A[SIZE] = {14, 13, 5, 16,
 
 int distance[SIZE];
 
-
 pthread_barrier_t   barrier; // barrier synchronization object
 
 void *jump(void *threadid)
 {
-   int i;
-   long tid;
+	int i;
+	long tid;
+	tid = (long)threadid;
    
-   tid = (long)threadid;
-   //printf("Hello World! It's me, thread #%ld!\n", tid);
-   
-  for (i = 0; i < 5; i++)
-  {
-    if (i==0) 
+	for (i = 0; i < 5; i++)
+	{
+    	if (i==0) //first step inits
 		{
-			if (A[tid] != 0)  distance[tid] = 1;
-			           	else distance[tid] = 0;
- 		}
-    else
-		{
-			A[tid] = A[A[tid]];
-            distance[tid] = distance[tid] + distance[A[tid]];
+			if (A[tid] != 0) distance[tid] = 1; //init distances
+			else 			 distance[tid] = 0; //root node	
+			A[tid] = A[tid]-1; //indicies correction
 		}
-
-      pthread_barrier_wait(&barrier);
- }
-     pthread_exit(NULL);
+    	else if (A[tid] != -1)
+		{
+			distance[tid] = distance[tid] + distance[A[tid]];
+			A[tid] = A[A[tid]];
+		}
+		pthread_barrier_wait(&barrier);
+	}
+	pthread_exit(NULL);
 }
 
 int main (int argc, char ** argv)
 {
- int i;
- printf ("Assignment C:  Sequential Algorithm! \n" );
- pthread_barrier_init (&barrier, NULL, NUM_THREADS);
+	int i;
+	printf ("Assignment C:  Sequential Algorithm! \n" );
+	pthread_barrier_init (&barrier, NULL, NUM_THREADS);
 
 //Create all threads
-  pthread_t threads[NUM_THREADS];
-   int rc;
-   long t;
-   for(t=0; t<NUM_THREADS; t++){
-     // printf("In main: creating thread %ld\n", t);
-      rc = pthread_create(&threads[t], NULL, jump, (void *)t);
-      if (rc){
-         printf("ERROR; return code from pthread_create() is %d\n", rc);
-         exit(-1);
-      }
-   }
+	pthread_t threads[NUM_THREADS];
+	int rc;
+	long t;
+	for(t=0; t<NUM_THREADS; t++)
+	{
+		rc = pthread_create(&threads[t], NULL, jump, (void *)t);
+		if (rc)
+		{
+			printf("ERROR; return code from pthread_create() is %d\n", rc);
+			exit(-1);
+		}
+	}
 
 //Join all threads
-for(t=0; t<NUM_THREADS/2; t++){
-      rc = pthread_join(threads[t], NULL);
-      if (rc){
-         printf("ERROR; return code from pthread_join() is %d\n", rc);
-         exit(-1);
-      }
-   }
+	for(t=0; t<NUM_THREADS; t++)
+	{
+		rc = pthread_join(threads[t], NULL);
+		if (rc)
+		{
+			printf("ERROR; return code from pthread_join() is %d\n", rc);
+			exit(-1);
+      	}
+	}
 
 	//Print all results
 	for (i = 0; i<SIZE; i++ )
@@ -79,8 +76,7 @@ for(t=0; t<NUM_THREADS/2; t++){
 	}
 	printf("\n");
    
-
-   pthread_barrier_destroy(& barrier );
-   pthread_exit(NULL);
-   return 0 ;
+	pthread_barrier_destroy(& barrier);
+	pthread_exit(NULL);
+	return 0 ;
 }
