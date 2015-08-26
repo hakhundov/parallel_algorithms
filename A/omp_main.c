5c248e6449524fd8de57168c2a7b780b59da2be6
Haji <hajiakhundov@gmail.com>
2015-08-26 13:57:22 +0200

    ex A, omp done

---
+++ A/omp_main.c
@@ -0,0 +1,162 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <omp.h>
+
+#define SIZE 32
+//#define NUM_THREADS 2
+#define NT 32 
+
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+
+
+int A[SIZE] = {58,  89,   32, 73,  131, 156, 30,  29, 
+         141, 37,  133, 151, 88,  53,  122, 126,
+         131, 49,  130, 115, 16,  83,  40,  145,
+         10,  112, 20,  147, 14,  104, 111, 92};
+
+int B[6][32]; // LOG2(SIZE) + 1 
+int C[6][32]; // LOG2(SIZE) + 1 
+int S[6][32]; // LOG2(SIZE) + 1 
+
+
+int h; //global for STEP2&3
+void execute(int num_threads, void * (*func)(void *)) {
+	int rc;
+	long t;
+	
+	#pragma omp parallel shared(A,B,C) private(t)
+	{
+		#pragma omp parallel for
+		for(t=0; t<num_threads; t++)
+		{
+			func((void *)t);
+		}
+	}
+}
+
+//initialize B[0,for all] = A[for all]
+void *init(void *threadid)
+{
+	long tid;
+	tid = (long)threadid;
+    B[0][tid] = A[tid];
+}
+
+void *R_init(void *threadid)
+{
+	long tid;
+	tid = (long)threadid;
+    B[0][SIZE-tid-1] = A[tid];
+}
+
+//balanced tree method
+void *minimum(void *threadid)
+{
+	long tid;
+	tid = (long)threadid;
+    B[h][tid] = min (  B[h-1][2*tid], B[h-1][2*tid+1]  );
+}
+
+void *combine(void *threadid)
+{
+	int i;
+	long tid;
+	tid = (long)threadid;
+   
+	if (tid == 0)
+	{
+		C[h][0] = B[h][0];
+	}
+	else if (tid%2 == 0)
+	{
+		C[h][tid] = min( C[h+1][(tid-1)/2], B[h][tid]  );
+	}
+	else
+	{
+		C[h][tid] = C[h+1][tid/2];
+	}
+}
+
+
+int main (int argc, char ** argv)
+{
+	int i;
+	int n_threads;
+
+	printf ("Assignment A:  pthread Algorithm! \n" );
+
+omp_set_num_threads(8);
+
+/*
+#pragma omp parallel
+{
+//	printf("I am using %d  threads", omp_get_num_threads());
+
+printf ("^^^HELLO I AM A THREAD!^^^");
+}
+*/
+
+
+//STEP 1
+	execute(NT, init);
+	
+//STEP2
+	for (h=1; h<=5; h++)   // REPLACE WITH LOG2 here!!!
+	{
+		n_threads = SIZE/(int)pow(2,h);
+		execute(n_threads,minimum);
+	}
+
+//STEP3
+	for (h=5; h>=0; h--)   // REPLACE WITH LOG2 here!!!
+	{
+		n_threads = SIZE/(int)pow(2,h);
+		execute(n_threads,combine);
+	}
+
+//Print all results
+	printf("prefix minimum is: \n");
+	for (i = 0; i<SIZE; i++ )
+	{
+		printf("%d ", C[0][i]);
+	}
+	printf("\n");
+
+
+
+
+//DO THE PREFIX NOW, by rotating
+
+//STEP 1
+	execute(NT, R_init); //<--- REVERSE INITIALIZATION
+	
+//STEP2
+	for (h=1; h<=5; h++)   // REPLACE WITH LOG2 here!!!
+	{
+		n_threads = SIZE/(int)pow(2,h);
+		execute(n_threads,minimum);
+	}
+
+//STEP3
+	for (h=5; h>=0; h--)   // REPLACE WITH LOG2 here!!!
+	{
+		n_threads = SIZE/(int)pow(2,h);
+		execute(n_threads,combine);
+	}
+
+
+//Print all results
+	printf("suffix minimum  is: \n");
+	for (i = 0; i<SIZE; i++ )
+	{
+		printf("%d ", C[0][SIZE-i-1]);
+	}
+	printf("\n");
+
+
+
+ 
+
+	return 0 ;
+}
